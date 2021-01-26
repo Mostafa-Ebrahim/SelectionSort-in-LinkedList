5cbb2de24b925f73b01f1e9e8952ffbbb82d9f8c
Mostafa Ebrahim <51707892+Mostafa-Ebrahim@users.noreply.github.com>
2021-01-26 12:35:24 +0200

    Update LinkedList.py

--- LinkedList.py
+++ LinkedList.py
@@ -5,41 +5,77 @@ class Node:
         self.data = data
         self.next = None
 
+
 class LinkedList:
     def __init__(self):
         self.head = None
 
-    def addv(self, data):
+    def insert(self, data):
         node = Node(data)
         node.next = self.head
         self.head = node
 
     def printlist(self):
         temp = self.head
-        ls = []
+        ll = []
         while temp:
-            ls.append(temp.data)
+            ll.append(temp.data)
             temp = temp.next
-        print(ls)
+        print(ll)
+
+    def reverse(self):
+        def _reverse(temp):
+            if temp is None:
+                return
+            else:
+                _reverse(temp.next)
+                print(temp.data)
+
+        _reverse(self.head)
 
     def selsort(self):
+        prevcur = None
         cur = self.head
         while cur.next is not None:
-            min = cur
-            cmp = cur.next
-            while cmp is not None:
-                if cmp.data < min.data:
-                    min = cmp
-                cmp = cmp.next
-
-            min.data, cur.data = cur.data, min.data
+            prevmin = cur
+            min = cur.data
+            minpos = cur
+            ismin = cur.next
+            while ismin is not None:
+                if ismin.data < min:
+                    min = ismin.data
+                    minpos = ismin
+                prevmin = ismin
+                ismin = ismin.next
+            self.swap(cur, prevcur, ismin, prevmin)
+            #minpos.data, cur.data = cur.data, minpos.data
+            prevcur = cur
             cur = cur.next
 
+    def swap(self, cur, prevcur, ismin, prevmin):
+        # If x is not head of linked list
+        if prevcur is not None:
+            prevcur.next = ismin
+        else:  # make y the new head
+            self.head = ismin
 
-ls = LinkedList()
+        # If y is not head of linked list
+        if prevmin is not None:
+            prevmin.next = cur
+        else:  # make x the new head
+            self.head = cur
 
-for i in range (10):
-    ls.addv(randrange(50))
-ls.printlist()
-ls.selsort()
-ls.printlist()
+        # Swap next pointers
+        temp = cur.next
+        cur.next = ismin.next
+        ismin.next = temp
+
+
+list0 = LinkedList()
+
+for i in range(10):
+    list0.insert(randrange(50))
+
+list0.printlist()
+list0.selsort()
+list0.printlist()
